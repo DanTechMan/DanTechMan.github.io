404284a9ac66e9768dbd97470380a2732e936703
DanTechMan <deeany87@gmail.com>
2024-06-21 23:49:42 +0100

    Add files via upload

---
+++ process_form.php
@@ -0,0 +1,37 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sanitize input data
+    $name = filter_var(trim($_POST["name"]), FILTER_SANITIZE_STRING);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $subject = filter_var(trim($_POST["subject"]), FILTER_SANITIZE_STRING);
+    $message = filter_var(trim($_POST["message"]), FILTER_SANITIZE_STRING);
+
+    // Validate input data
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email format";
+        exit;
+    }
+
+    // Prepare email
+    $to = "deeany87@gmail.com";
+    $email_subject = "Contact Form Submission: $subject";
+    $email_body = "You have received a new message from the contact form on your website.\n\n".
+                  "Here are the details:\n".
+                  "Name: $name\n".
+                  "Email: $email\n".
+                  "Subject: $subject\n".
+                  "Message:\n$message";
+
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Send email
+    if (mail($to, $email_subject, $email_body, $headers)) {
+        echo "Thank you! Your message has been sent.";
+    } else {
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+} else {
+    echo "There was a problem with your submission. Please try again.";
+}
+?>
